e26978af35a14d7931afccb4281678c36f417d28
sushachawal <ssa43@cam.ac.uk>
2018-11-27 16:07:45 +0000

    Delete bttest and replace with 4p2 exercise

--- 4p2.cpp
+++ 4p2.cpp
@@ -1,7 +1,10 @@
 #include "BinaryTree.h"
 
-int main(int argc, char** argv[]){
-  int array[] = {1};
+int main(int argc, const char* argv[]){
+  int array[argc-1];
+  for(int i = 1; i < argc ; i++){
+    array[i-1] = atoi(argv[i]);
+  }
   int size = sizeof(array)/sizeof(*array);
   BTNode* q = sorted_array_to_BST(array, size);
   inOrderTraversal(q);
